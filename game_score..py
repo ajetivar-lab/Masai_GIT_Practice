0e7cf3760d2b131f3dec960d4005ae138d645e3a
ajetivar-lab <dontojuravi@gmail.com>
2026-02-06 15:57:26 +0530

    Update game_score..py

---
+++ game_score..py
@@ -0,0 +1,5 @@
+player = input("Player : ")
+games_played=int(input("Games Played : "))
+total_score=int(input("Total Scored : "))
+Average_score=total_score/games_played
+print("Average Score: ", Average_score)
